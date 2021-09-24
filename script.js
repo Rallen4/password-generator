1fa585a2b37f24ec4ac75b84abe422a0d5f763e2
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-09-23 19:50:15 -0700

    defined js variables to correspond with character number and button clicking

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var lowcaseChar
-var upcaseChar
-var numChar
-var specChar = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-function generatePassword {
-    
-}
+var passOutput = document.getElementById("output");
+var charlength = document.getElementById("length");
+var useNumbers = document.getElementById("NUMBERS");
+var useSymbols = document.getElementById("SYMBOLS");
+var genButton = document.getElementById("button");
+var form = document.getElementById("pwdGen");
+
+
+
+
